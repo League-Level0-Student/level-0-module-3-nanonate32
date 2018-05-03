83b233601eacf7837442421800abe870e7e6c83a
nanonate32 <league@wts14.attlocal.net>
2018-05-02 19:56:46 -0700

    fizzbuzz oddlyloopy bottles

---
+++ src/bottles.java
@@ -0,0 +1,12 @@
+
+public class bottles {
+
+	public static void main(String[] args) {
+		for (int i = 99; i > 0; i--) {
+			System.out.println(i + " bottles of milk on the wall," + i
+					+ " bottles of milk.Take one down, pass it around, " + (i - 1) + " bottles of milk on the wall.");
+		}
+
+	}
+
+}
